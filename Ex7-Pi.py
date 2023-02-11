fe81b5d9e5df9422872b94aff13a47694abad034
nitzanXcohen <107773441+nitzanXcohen@users.noreply.github.com>
2023-02-11 18:25:40 +0200

    Update Ex7-Pi.py

--- Ex7-Pi.py
+++ Ex7-Pi.py
@@ -17,20 +17,12 @@ def compute_pi():
         print("Calculated value of Pi: ", pi_estimate)
         print("Number of attempts: ", i * trials_per_batch + trials_per_batch)
 compute_thread = threading.Thread(target=compute_pi)
-input("Press enter to start the calculation...")
+input("Press enter to start the calculation")
 compute_thread.start()
-
-input("Press enter to stop the calculation...")
-
+input("Press enter to stop the calculation")
 compute_thread.join()
-
-# print final result
-
 pi_estimate = 4 * points_inside / (num_batches * trials_per_batch)
-
 print("Real value of Pi: 3.14...")
-
 print("Calculated value of Pi: ", pi_estimate)
-
 print("Number of attempts: ", num_batches * trials_per_batch)
 
