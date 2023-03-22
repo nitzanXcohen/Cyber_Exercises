21118d6684ffbe005bcab0e947f855715f31725d
nitzanXcohen <107773441+nitzanXcohen@users.noreply.github.com>
2023-03-22 02:15:25 +0200

    Update BULLS & COWS - 3.py

--- BULLS & COWS - 3.py
+++ BULLS & COWS - 3.py
@@ -58,11 +58,14 @@ if __name__ == '__main__':
 
 
 
+
+
+
+
 #2
 import argparse
 import random
 import socket
-import SocketServer
 
 parser = argparse.ArgumentParser()
 parser.add_argument("number", help="the secret number")
@@ -71,66 +74,67 @@ args = parser.parse_args()
 chosen = args.number
 
 def is_valid(num):
- digits = '123456789'
- return len(num) == 4 and \
-  all(char in digits for char in num) \
-  and len(set(num)) == 4
+    digits = '123456789'
+    return len(num) == 4 and \
+        all(char in digits for char in num) \
+        and len(set(num)) == 4
 
 won = False
 
-class MyUDPHandler(SocketServer.BaseRequestHandler):
-    def handle(self):
-        data = self.request[0].strip()
-        socket = self.request[1]
-        raw = data.split(':')
-        if (raw[0] != 'GUESS'):
-           socket.sendto('BAD FORMAT', self.client_address)
-           return
-        guess = raw[1].strip()
-        if not is_valid(guess):
-         socket.sendto('BAD NUMBER', self.client_address)
-        if int(guess) == int(chosen):
-         socket.sendto('WIN', self.client_address)
-	 global won
-         won = True
-         return
-        bulls = cows = 0
-        for i in range(4):
-         if guess[i] == chosen[i]:
+def handle_guess(sock, client_address, data):
+    raw = data.split(':')
+    if (raw[0] != 'GUESS'):
+        sock.sendto('BAD FORMAT'.encode(), client_address)
+        return
+    guess = raw[1].strip()
+    if not is_valid(guess):
+        sock.sendto('BAD NUMBER'.encode(), client_address)
+        return
+    if int(guess) == int(chosen):
+        sock.sendto('WIN'.encode(), client_address)
+        global won
+        won = True
+        return
+    bulls = cows = 0
+    for i in range(4):
+        if guess[i] == chosen[i]:
             bulls += 1
-         elif guess[i] in chosen:
+        elif guess[i] in chosen:
             cows += 1
-        socket.sendto('%iB%iC' % (bulls, cows), self.client_address)
+    sock.sendto('%iB%iC' % (bulls, cows), client_address)
 
+def server():
+    if not is_valid(chosen):
+        print ('SECRET IS NOT VALID')
+        return
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((HOST, int(args.my_port)))
+    print(f"Server started on port {args.my_port}")
+    while not won:
+        data, client_address = sock.recvfrom(1024)
+        handle_guess(sock, client_address, data)
+
+def client():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(1)
+    print(f"Client started on port {args.my_port}")
+    for i in range(10):
+        myguess = input("Enter a 4-digit number: ")
+        while not is_valid(myguess):
+            myguess = input("Invalid input, please enter a valid 4-digit number: ")
+        sock.sendto('GUESS:' + myguess, (HOST, int(args.my_port)))
+        try:
+            received, server_address = sock.recvfrom(1024)
+            print(f"Result: {received.decode()}")
+            if "{}".format(received.decode()) == 'WIN':
+                print("You won!")
+                return
+        except socket.timeout:
+            print("No response from server.")
+            return
 
 HOST, PORT = "localhost", int(args.my_port)
 
-def client():
- sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- sock.settimeout(1)
- for a in range(1, 10):
-    for b in range(1, 10):
-     for c in range(1, 10):
-      for d in range(1, 10):
-       if len(set([a,b,c,d])) < 4:
-        continue
-       myguess = ''.join(map(str, [a, b, c, d]))
-       sock.sendto('GUESS:' + myguess, (HOST, PORT))
-       received = sock.recv(1024)
-       print ("Guessed:  " + str(myguess))
-       print ("Received: {}\n".format(received))
-       if "{}".format(received) == 'WIN':
-        return
-
-def server():
- if not is_valid(chosen):
-  print ('SECRET IS NOT VALID')
-  return
- server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
- while not won:
-  server.handle_request()
-
-try:
- client()
-except socket.timeout:
- server()
+if __name__ == '__main__':
+    server()
+    client()
